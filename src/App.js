1f193c88cd572cbf08f727dbb317bf2a85debcc0
Oleksandr Horobets <git.sparrow@gmail.com>
2019-09-12 19:32:40 +0300

    add 404 page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import HomePage from './components/HomePage'
 import EditNote from './components/EditNote'
 import ShowNote from './components/ShowNote'
+import NotFound from './components/NotFound404'
 
 import './App.css'
 
@@ -11,9 +12,12 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/edit" component={EditNote} />
-        <Route exact path="/show-note" component={ShowNote} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/edit" component={EditNote} />
+          <Route exact path="/show-note" component={ShowNote} />
+          <Route component={NotFound} />
+        </Switch>
       </Router>
     )
   }
