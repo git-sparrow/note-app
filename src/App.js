40701ea616efd9fbcf668dcb46f86a3b6ec35369
Oleksandr Horobets <git.sparrow@gmail.com>
2019-09-12 08:54:04 +0300

    demo version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    )
+  }
 }
 
-export default App;
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { items: [], text: '' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    if (!this.state.text.length) {
+      return
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now(),
+    }
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: '',
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Notes</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">What needs to be done?</label>
+          <input id="new-todo" onChange={this.handleChange} value={this.state.text} />
+          <button>Add #{this.state.items.length + 1}</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
